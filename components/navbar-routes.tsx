1831d66afd62a69b64fa36fe5bd63003daad5475
Andy Ramaroson <andyramaroson@gmail.com>
2023-09-20 19:38:23 +0200

    [enhancement] navbar-route and btn link for teacher dashboard

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,9 +1,35 @@
 "use client"
+
 import { UserButton } from "@clerk/nextjs"
+import { usePathname, useRouter } from "next/navigation"
+import { Button } from "./ui/button"
+import { LogOut } from "lucide-react"
+import Link from "next/link";
  
 export const NavbarRoutes = () => {
+  const pathname = usePathname()
+  const router = useRouter()
+
+  //Individual Teacher page
+  const isTeacherPage = pathname?.startsWith("/teacher")
+
+  //Individual Course page
+  const isPlayerPage = pathname?.includes("/chapter")
+
   return (
     <div className="flex gap-x-2 ml-auto">
+      {isTeacherPage || isPlayerPage ? (
+        <Button size="sm" variant="ghost" >
+          <LogOut className="border h-4 w-4 mr-2"/> 
+          Exit
+        </Button> 
+      ) : (
+        <Link href="/teacher/course">
+          <Button size="sm" variant="ghost">
+            Teacher mode
+          </Button>
+        </Link>
+      )}
       <UserButton/>
     </div>
   )
