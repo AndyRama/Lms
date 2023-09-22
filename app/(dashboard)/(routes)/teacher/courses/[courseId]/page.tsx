f656f1e1ad46f8931f2ed17bfefe652ffbc26391
Andy Ramaroson <andyramaroson@gmail.com>
2023-09-22 23:31:47 +0200

    [enhancement]create sectionfi first section of course and add icon badge

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -22,11 +22,40 @@ const CourseIdPage = async ({
   if(!course) {
     return redirect("/")
   }
+  
+  const requiredField = [
+    course.title,
+    course.description,
+    course.imageUrl,  
+    course.price,
+    course.categoryId,
+  ]
+
+  const totalFields = requiredField.length
+  const completedFields = requiredField.filter(Boolean).length
+  const completionText =`(${completedFields} / ${totalFields})` 
 
   return ( 
-    <div>
-      CourseId: { params.courseId}
+    <div className="p-6">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-medium">Course setup</h1>
+          <span className="text-sm text-slate-700">
+            Complete all fields {completionText}
+          </span>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">  
+        <div>
+          <div className="flex items-center gap-x-2">
+            <h2 className="text-xl">
+              Customize you course
+            </h2>
+          </div>
+        </div>
+      </div>
     </div>
+
    );
 }
  
