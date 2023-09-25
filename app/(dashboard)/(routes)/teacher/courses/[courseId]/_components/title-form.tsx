492b8711e435dbd96e3f067d486089bceae370e2
Andy Ramaroson <andyramaroson@gmail.com>
2023-09-25 09:28:33 +0200

    [enhancmeent] Add request patch for update title and add toast

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
@@ -7,8 +7,10 @@ import { useForm } from "react-hook-form"
 
 import {Input} from "@/components/ui/input"
 import {Button} from "@/components/ui/button"
-import { Pencil } from "lucide-react"
+import { Pencil, Router } from "lucide-react"
 import { useState } from "react"
+import toast from "react-hot-toast"
+import{ useRouter} from "next/navigation"
 
 import {
   Form,
@@ -40,7 +42,7 @@ export const TitleForm = ({
   const [isEditing, setEditing] = useState(false)
 
   const toggleEdit = () => setEditing((current) => !current)
-// =>
+  const router = useRouter()
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -50,14 +52,17 @@ export const TitleForm = ({
   const {isSubmitting, isValid} = form.formState
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
-    // try {
-      
-    // } catch (error) {
-      
-    // }
-    
+    // console.log(values);
+    try {
+      await axios.patch(`api/course/${courseId}`, values)
+      toast.success("Course Title updated") 
+      toggleEdit()
+      router.refresh()     
+    } catch {
+      toast.error("Something not work !")      
+    }    
   }
+
   return(
     <div className="mt-6 border bg-slate-100 rounded-md p-4">
       <div className="font-medium flex items-center justify-between">
@@ -98,10 +103,17 @@ export const TitleForm = ({
                 </FormItem>
               )}
             />
+            <div className="flex items-center gap-x-2">
+              <Button
+                disabled={ !isValid || isSubmitting }
+                type="submit"
+              >
+                Save               
+              </Button>
+            </div>
           </form>
-
         </Form>
-      )     }      
+      )}      
     </div>
   )
 }
