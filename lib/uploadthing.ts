84b4ebebff5197ae19a8a8202e07fd6e7a520a59
Andy Ramaroson <andyramaroson@gmail.com>
2023-09-27 10:02:19 +0200

    [enhancement] add file-uploadthing and lib/uploadthing

---
+++ lib/uploadthing.ts
@@ -0,0 +1,6 @@
+import { generateComponents } from "@uploadthing/react";
+ 
+import type { OurFileRouter } from "@/app/api/courses/uploadthing/core";
+ 
+export const { UploadButton, UploadDropzone, Uploader } =
+  generateComponents<OurFileRouter>();
