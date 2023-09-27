79f3167c128eeb61447bb2ab6aeffa7342c5e35a
Andy Ramaroson <andyramaroson@gmail.com>
2023-09-27 11:09:32 +0200

    [enhancement] Add content when upload a file, update tailwind.config and add import in global.css

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/image-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/image-form.tsx
@@ -8,21 +8,13 @@ import { useForm } from "react-hook-form";
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Pencil, PlusCircle } from "lucide-react";
+import { ImageIcon, Pencil, PlusCircle } from "lucide-react";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormMessage,
-} from "@/components/ui/form";
-
-import { cn } from "@/lib/utils";
-import { Textarea } from "@/components/ui/textarea";
+import Image from "next/image";
+import { FileUpload } from "@/components/file-upload";
 
 interface ImageFormProps {
   initialData: Course;
@@ -92,46 +84,37 @@ export const ImageForm = ({
       </div>
 
       {!isEditing && (
-        <p className= {cn(
-          "text-sm mt-2",
-          !initialData.imageUrl && "text-slate-500"
-        )}>
-          {initialData.imageUrl ||" No image"}
-        </p>
+        !initialData.imageUrl ? (
+          <div className="flex items-center justify-center h-60
+           bg-slate-200 rounded-md">
+            <ImageIcon className="h-10 w-10 text-slate-500 "/>
+          </div>
+        ) : (
+          <div className="relative aspect-video mt-2">
+            <Image
+              alt="Upload"
+              fill
+              className="object-cover rounded-md"
+              src={initialData.imageUrl}
+            />
+          </div>
+        )
       )}
 
-      {isEditing && (
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="space-y-4 mt-4"
-          >
-            <FormField
-              control={form.control}
-              name="imageUrl"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Textarea
-                      disabled={isSubmitting}
-                      placeholder= "e.g. ' This course image'"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <div className="flex items-center gap-x-2">
-              <Button
-                disabled={!isValid || isSubmitting}
-                type="submit"
-              >
-                Save
-              </Button>
-            </div>
-          </form>
-        </Form>
+     {isEditing && (
+        <div>
+          <FileUpload
+            endpoint="courseImage"
+            onChange={(url) => {
+              if (url) {
+                onSubmit({ imageUrl: url });
+              }
+            }}
+          />
+          <div className="text-xs text-muted-foreground mt-4">
+            16:9 aspect ratio recommended
+          </div>
+        </div>
       )}
     </div>
   )
