2d52e116fa659fc3cd3913294e5505c2b8bb5c04
Andy Ramaroson <andyramaroson@gmail.com>
2023-10-09 23:28:51 +0200

    [Enhancement] counter complete chapter

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,4 +1,7 @@
+import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 const ChapterIdPage = async ({
@@ -11,11 +14,50 @@ const ChapterIdPage = async ({
   if(!userId) {
     return redirect("/")
   }
+
+  const chapter = await db.chapter.findUnique ({
+    where: {
+      id: params.chapterId,
+      courseId: params.courseId,
+    },
+    include: {
+      muxData: true
+    }
+  })
+  
+  if(!chapter) {
+    return redirect("/")
+  }
+
+  const requiredField = [
+    chapter.title,
+    chapter.description,
+    chapter.videoUrl
+  ]
+
+  const totalFields = requiredField.length
+
+  const completedFields = requiredField.filter(Boolean).length
+
+  const completionText = `(${ completedFields}/${totalFields})`
+
   return (
-    <div>
+    <div className="p-6">
+      <div className="flex items-center justify-between">
+        <div className="w-full">
+          <Link
+            href={`/teacher/course/${params.courseId}`}
+            className="flex items-center text-sm hover:opacity-75 transition mb-6"
+          >
+            <ArrowLeft className="w-4 h-4 mr-2"/>
+            Back to course setup
+          </Link>
+        </div>
+      </div>
       Chapter Id
     </div>
   )
 }
+
 
 export default ChapterIdPage
