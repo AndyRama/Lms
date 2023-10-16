44ac4eea22529892faab94151797203a8d153610
Andy Ramaroson <andyramaroson@gmail.com>
2023-10-16 11:08:10 +0200

    [enhancement] Add Bannerprops  and create components chapter-actions

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -10,6 +10,8 @@ import { ChapterTitleForm } from "./_components/chapter-title-form";
 import { ChapterDescriptionForm } from "./_components/chapter-description-form";
 import { ChapterAccessForm } from "./_components/chapter-access-form";
 import { ChapterVideoForm } from "./_components/chapter-video-form";
+import { Banner } from "@/components/banner";
+import { ChapterActions } from "./_components/chapter-actions";
 
 const ChapterIdPage = async ({
   params
@@ -48,76 +50,92 @@ const ChapterIdPage = async ({
 
   const completionText = `(${ completedFields}/${totalFields})`
 
-  return (
-    <div className="p-6">
-      <div className="flex items-center justify-between">
-        <div className="w-full">
-          <Link
-            href={`/teacher/course/${params.courseId}`}
-            className="flex items-center text-sm hover:opacity-75 transition mb-6"
-          >
-            <ArrowLeft className="w-4 h-4 mr-2"/>
-            Back to course setup
-          </Link>
+  const isComplete = requiredField.every(Boolean)
 
-          <div className="flex items-center justify-between w-full">
-            <div className="flex flex-col gap-y-2">
-             <h1 className="text-2xl font-medium">
-              Chapter Creation
-             </h1>
-             <span className="text-sm text-slate-700">
-                Complete all field {completionText}
-             </span>
+  return (
+    <>
+      {!chapter.isPublished && (
+        <Banner
+          variant="warning"
+          label="this chapter is published. It will not visible in the course"
+        />
+      )}
+      <div className="p-6">
+        <div className="flex items-center justify-between">
+          <div className="w-full">
+            <Link
+              href={`/teacher/course/${params.courseId}`}
+              className="flex items-center text-sm hover:opacity-75 transition mb-6"
+            >
+              <ArrowLeft className="w-4 h-4 mr-2"/>
+              Back to course setup
+            </Link>
+
+            <div className="flex items-center justify-between w-full">
+              <div className="flex flex-col gap-y-2">
+              <h1 className="text-2xl font-medium">
+                Chapter Creation
+              </h1>
+              <span className="text-sm text-slate-700">
+                  Complete all field {completionText}
+              </span>
+              </div>
+              <ChapterActions
+                disabled={!isComplete}
+                courseId={params.courseId}
+                chapterId={params.chapterId}
+                isPublished={chapter.isPublished}
+              />
             </div>
           </div>
         </div>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
-        <div className="space-y-4">
-          <div>
-            <div className="flex items-center gap-x-2">
-              <IconBadge icon={LayoutDashboard} />
-              <h2 className="text-xl">
-                Customize your chapter
-              </h2>
-            </div>
-            <ChapterTitleForm
-              initialData={chapter}
-              courseId={params.courseId}
-              chapterId={params.chapterId}
-            />
-            <ChapterDescriptionForm
-              initialData={chapter}
-              courseId={params.courseId}
-              chapterId={params.chapterId}
-            />
-          </div>
-          <div>
-            <div className="flex items-centrer gap-x-2">
-              <IconBadge icon={Eye}/>
-              <h2 className="text-xl">
-                Access Settings
-              </h2>
-              <ChapterAccessForm 
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+          <div className="space-y-4">
+            <div>
+              <div className="flex items-center gap-x-2">
+                <IconBadge icon={LayoutDashboard} />
+                <h2 className="text-xl">
+                  Customize your chapter
+                </h2>
+              </div>
+              <ChapterTitleForm
+                initialData={chapter}
+                courseId={params.courseId}
+                chapterId={params.chapterId}
+              />
+              <ChapterDescriptionForm
                 initialData={chapter}
                 courseId={params.courseId}
                 chapterId={params.chapterId}
               />
             </div>
+            <div>
+              <div className="flex items-centrer gap-x-2">
+                <IconBadge icon={Eye}/>
+                <h2 className="text-xl">
+                  Access Settings
+                </h2>
+                <ChapterAccessForm 
+                  initialData={chapter}
+                  courseId={params.courseId}
+                  chapterId={params.chapterId}
+                />
+              </div>
+            </div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={Video}/>
+              <h2 className="text-xl">
+                Add a video
+              </h2>
+            </div>
+            <ChapterVideoForm
+              initialData={chapter}
+              courseId={params.courseId} 
+              chapterId={params.chapterId}/>
           </div>
-          <div className="flex items-center gap-x-2">
-            <IconBadge icon={Video}/>
-            <h2 className="text-xl">
-              Add a video
-            </h2>
-          </div>
-          <ChapterVideoForm
-             initialData={chapter}
-             courseId={params.courseId} 
-             chapterId={params.chapterId}/>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
--- components/banner.tsx
+++ components/banner.tsx
@@ -9,13 +9,35 @@ const bannerVariants = cva(
     variants: {
       variant: {
         warning: "bg-yellow-200/80 border-yellow-30 text-primary",
-        success: ""
+        success: "bg-emerald-700 border-emerald-800 text-secondary",
       }
+    },
+    defaultVariants: {
+      variant: "warning",
     }
   }
 )
-export const Banner = () => {
+
+interface BannerProps extends VariantProps<typeof bannerVariants> {
+  label: string
+}
+
+const iconMap = {
+  warning: AlertTriangle,
+  success: CheckCircleIcon,
+}
+
+export const Banner = ({
+  label,
+  variant,
+ } : BannerProps)  => {
+
+  const Icon = iconMap [variant || "warning"]
+  
   return (
-    <div>Banner</div>
+    <div className={cn(bannerVariants({ variant }))}>
+      <Icon className="h-4 w-4 mr-2"/>
+       {label}
+    </div>
   )
 }
