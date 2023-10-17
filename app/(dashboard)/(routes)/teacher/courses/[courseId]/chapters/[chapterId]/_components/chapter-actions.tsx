d1020bea86e006caf3670aa3759af8cfbb438183
Andy Ramaroson <andyramaroson@gmail.com>
2023-10-17 22:34:37 +0200

    [enhancement] chapter delete methode

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
@@ -3,6 +3,10 @@
 import { Button } from "@/components/ui/button"
 import { Trash } from "lucide-react"
 import { ConfirmModal } from "@/components/modals/confirm-modal";
+import { useState } from "react";
+import { toast } from "react-hot-toast";
+import axios from "axios";
+
 
 interface ChapterActionsProps {
   disabled: boolean,
@@ -17,6 +21,24 @@ export const ChapterActions = ({
   chapterId,
   isPublished,
 }: ChapterActionsProps) => {
+  
+  const[isLoading, setIsLoading] = useState(false)
+  
+  const onDelete = async () => {
+
+    try {
+      setIsLoading(true)
+
+      await axios.delete(`/api/course/${courseId}/chapters/${chapterId}`)
+
+      toast.success("Chapter deleted")
+
+    } catch {
+      toast.error("Something went wrong")
+    } finally {
+      setIsLoading(false)
+    }
+  }
 
   return(
     <div className="flex items-center gp-x-2">
