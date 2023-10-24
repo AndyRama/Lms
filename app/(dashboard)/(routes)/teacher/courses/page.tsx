9c3b937400b1ef2baac39697dc3a4b3ad115719c
Andy Ramaroson <andyramaroson@gmail.com>
2023-10-24 23:11:02 +0200

    [fix] Error fix fetch data course in table

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -4,7 +4,7 @@ import { auth } from "@clerk/nextjs";
 
 import { DataTable } from "./_components/data-table";
 import { columns } from "./_components/columns";
-import { redirect } from "next/dist/server/api-utils";
+import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
 
 const CoursesPage = async () => {
@@ -16,15 +16,16 @@ const CoursesPage = async () => {
 
   const courses = await db.course.findMany({
     where: {
-      userId
+      userId,
     }, 
     orderBy: {
-      createdAt:"desc"
-    }
+      createdAt:"desc",
+    },
   })
+
   return ( 
     <div className="p-6">
-      <DataTable columns={columns} data={data} />
+      <DataTable columns={columns} data={courses} />
     </div>
    );
 }
