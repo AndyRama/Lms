4236b379422ba28c91aadb8df9b33a2f23cd7347
Andy Ramaroson <andyramaroson@gmail.com>
2023-10-25 22:42:13 +0200

    [enhancement] add btn sortering table for price and publish

--- app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -20,14 +20,36 @@ export const columns: ColumnDef<Course>[] = [
           <ArrowUpDown className="ml-2 h-4 w-4"/>
         </Button>
       )
-    }
+    },
   },
   {
     accessorKey: "price",
-    header: "Price",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(
+            column.getIsSorted() === "asc")}
+        >
+          Price
+          <ArrowUpDown className="ml-2 h-4 w-4"/>
+        </Button>
+      )
+    }
   },
   {
     accessorKey: "isPublished",
-    header: "Published",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(
+            column.getIsSorted() === "asc")}
+        >
+          Publish
+          <ArrowUpDown className="ml-2 h-4 w-4"/>
+        </Button>
+      )
+    }
   },
 ]
