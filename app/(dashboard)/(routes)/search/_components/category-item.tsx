b531bbe4d3253edb7b7c1171e4ad9eecb7d0c3b8
Andy Ramaroson <andyramaroson@gmail.com>
2023-10-30 22:24:34 +0100

    [enhancement] add pathname, router and searchParams

--- app/(dashboard)/(routes)/search/_components/category-item.tsx
+++ app/(dashboard)/(routes)/search/_components/category-item.tsx
@@ -3,6 +3,13 @@
 import { cn } from "@/lib/utils";
 import { IconType } from "react-icons";
 
+import { 
+  usePathname, 
+  useRouter, 
+  useSearchParams
+} from "next/navigation";
+
+
 interface CategoryItemProps {
   label: string;
   value?: string;
@@ -14,8 +21,17 @@ export const CategoryItem = ({
   value,
   icon: Icon
 } : CategoryItemProps) => {
+
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const currentCategoryId = searchParams.get("categoryId");const currentTitle = searchParams.get("title");
+
+  const isSelected = currentCategoryId === value;
   return (
-    <button className={cn("py-2 px-3 text-sm border border-slate-200 rounded-full flex items-center gap-x-1 hover:border-sky-700 transition,")}>
+    <button className={cn(
+      "py-2 px-3 text-sm border border-slate-200 rounded-full flex items-center gap-x-1 hover:border-sky-700 transition")}>
       {Icon && <Icon size={20}/>}
         <div className="truncate">
           {label}
